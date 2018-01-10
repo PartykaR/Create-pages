ee45d3b3755bea0f38a8bf240450bf2ff5b6ef6c
PartykaR <partyka_robert@yahoo.com>
2018-01-10 19:33:10 +0100

    Reset scroll
    Link setting

--- js/index.js
+++ js/index.js
@@ -2,6 +2,28 @@ jQuery(document).ready(function()
 {
 	jQuery("#menu-responsive").click(function()
 	{
-		jQuery("header nav").fadeToggle(600);
+		jQuery("nav").fadeToggle(1000);
 	});
 });
+
+    jQuery(function($) {
+            //zresetuj scrolla
+            $.scrollTo(0);
+
+            $('#link1').click(function() {
+                $.scrollTo($('#oferta'), 500);
+            });
+            $('#link2').click(function() {
+                $.scrollTo($('#design'), 500);
+            });
+            $('#link3').click(function() {
+                $.scrollTo($('#omnie'), 500);
+            });
+            $('#link5').click(function() {
+                $.scrollTo($('#bitcoin'), 500);
+            });
+
+            $('.scrollup').click(function() {
+                $.scrollTo($('body'), 1000);
+            });
+        });
