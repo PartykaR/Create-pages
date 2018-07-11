93610363383d3cc8030e34ec444b2d3a9fbb0d5b
RobertPartyka <partyka_robert@yahoo.com>
2018-07-11 11:15:11 +0200

    The sticky menu has been fixed

--- js/sticky.js
+++ js/sticky.js
@@ -1,15 +1,15 @@
 //Menu sticky
 
 $(document).ready(function() {
-	var NavY = $('main #menu .nav').offset().top;
+	var NavY = $('.menu').offset().top;
 	 
 	var stickyNav = function(){
 	var ScrollY = $(window).scrollTop();
 		  
 	if (ScrollY > NavY) { 
-		$('main #menu .nav').addClass('sticky');
+		$('.menu').addClass('sticky');
 	} else {
-		$('main #menu .nav').removeClass('sticky'); 
+		$('.menu').removeClass('sticky'); 
 	}
 	};
 	 
