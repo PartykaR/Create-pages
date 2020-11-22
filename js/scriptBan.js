9d8a9ffccc714f7ceabafe4160c00e637cdc6b3a
Rudzik <freelancer.partyka@gmail.com>
2020-11-22 19:19:20 +0100

    I launched the slider

---
+++ js/scriptBan.js
@@ -0,0 +1,28 @@
+let activeElement = 0;
+const timeChange = 4000;
+
+const colorImgHtml = document.querySelector('.color');
+const grayImgHtml = document.querySelector('.gray');
+const h1Html = document.querySelector('.member h1');
+const h2Html = document.querySelector('.member h2');
+
+const colorImages = ['img/banSlider/s1.png', 'img/banSlider/s2.png', 'img/banSlider/s3.png'];
+const grayImages = ['img/banSlider/s1a.png', 'img/banSlider/s2a.png', 'img/banSlider/s3a.png'];
+const names = ['Anna Bugart', 'Robert Partyka', 'Arek Pawłowicz'];
+const profesions = ['Programistka JS', 'Front-End Developer', 'UX i UI Designer'];
+
+function changeElement() {
+   activeElement++;
+   if (activeElement >= colorImages.length) {
+      activeElement = 0;
+   }
+
+   colorImgHtml.src = colorImages[activeElement];
+   grayImgHtml.src = grayImages[activeElement];
+   h1Html.textContent = names[activeElement];
+   h2Html.textContent = profesions[activeElement];
+   console.log("aut wywoł");
+}
+
+setInterval(changeElement, timeChange)
+
